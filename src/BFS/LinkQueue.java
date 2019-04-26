6f9864b99a70de547dce0b193e66a629b8dd8492
LoseNine <charlesval@sina.com>
2019-04-26 20:19:19 +0800

    Priority crawl

--- src/BFS/LinkQueue.java
+++ src/BFS/LinkQueue.java
@@ -1,13 +1,19 @@
 package BFS;
 
 import java.util.HashSet;
+import java.util.PriorityQueue;
 import java.util.Set;
+import java.util.Queue;
 
 public class LinkQueue {
     //被访问的URL集合
     private static Set visitedUrl=new HashSet();
     //待访问的URL
-    private static Queue unVisitedUrl=new Queue();
+    //private static Queue unVisitedUrl=new Queue();
+
+    //更改为优先级队列，实现带偏好的爬虫
+    private static Queue unVisitedUrl= new PriorityQueue();
+
     //获取URL队列
     public static Queue getUnVisitedUrl(){
         return unVisitedUrl;
@@ -22,13 +28,15 @@ public class LinkQueue {
     }
     //未访问的URL出队列
     public static Object unVisitedUrlDeQueue(){
-        return unVisitedUrl.deQueu();
+        //return unVisitedUrl.deQueu();
+        return unVisitedUrl.poll();
     }
     //保证每个URL被访问一次
     public static void addUnvisitedUrl(String url){
         if(url!=null && !url.trim().equals("") &&
                 !visitedUrl.contains(url) && !unVisitedUrl.contains(url)){
-            unVisitedUrl.enQueue(url);
+            //unVisitedUrl.enQueue(url);
+            unVisitedUrl.add(url);
         }
     }
     //获取已访问的URL数目
@@ -37,6 +45,7 @@ public class LinkQueue {
     }
     //判断未访问的URL队列是否为空
     public static boolean unVisistedUrlEmpty(){
-        return unVisitedUrl.empty();
+        //return unVisitedUrl.empty();
+        return unVisitedUrl.isEmpty();
     }
 }
